336854badc6957ca3361d050cd1a42628f50255d
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-25 13:53:56 -0400

    basic server running

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,11 +13,17 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
 });
+
+app.get("/", (req, res) => {
+    res.render("index");
+    
+})
 
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
+  console.log(`http://localhost:${PORT}`);
 });
