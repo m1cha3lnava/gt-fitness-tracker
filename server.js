9aab40a7ad921b0cf95f874d41d2d93358e556b7
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-25 14:50:44 -0400

    basic server setup

--- server.js
+++ server.js
@@ -15,13 +15,19 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
+//view routes
 app.get("/", (req, res) => {
-    res.render("index");
-    
-})
+  res.render("index");
+});
+// post routes
+app.post("submit", ({ body }, res) => {
+  // const workout =
+});
+//update routes
 
+//delete routes
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
